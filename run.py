4cf8b9464124ad2a59b93d4cd0b4bdd9ae5b323c
lot.xu <754345848@qq.com>
2014-09-27 17:04:30 +0800

    20140927 update

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 from flask import Flask,render_template
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/index')
 def index():
-     return render_template("index.html", title = 'Home')
+    return render_template("index.html", title='Home')
 
+
+@app.route('/login')
+def login():
+    return render_template("login.html", title='Login')
 if __name__ == '__main__':
     app.run()
