1edcadf4ef61f7f896277ac1f64f157f499e82ae
lot.xu <754345848@qq.com>
2014-10-12 23:49:14 +0800

    20141012 add sqlite3 & reg

--- run.py
+++ run.py
@@ -1,7 +1,10 @@
-from flask import Flask, render_template, request, session, redirect, url_for, escape
+from flask import Flask, render_template, request, session, redirect, url_for, escape, flash
 import time
+import sqlite3
 app = Flask(__name__)
 
+JDB_NAME = 'JDB.db'
+#..\JetBrains\PyCharm 3.4.1\jre\jre\bin\JDB.db(the location)
 
 @app.route('/')
 @app.route('/index')
@@ -26,8 +29,30 @@ def logout():
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 
-@app.route('/reg')
+@app.route('/reg', methods=['GET', 'POST'])
 def reg():
+    error = None
+    if request.method == 'POST':
+        if request.form['password'] != request.form['password-repeat']:
+            error = 'Passwords do not match.'
+            return render_template("reg.html", title='Reg', error=error)
+        if request.form['username'].strip() == '':
+            error = 'UserName cannot be empty.'
+            return render_template("reg.html", title='Reg', error=error)
+        if request.form['password'] == '' or request.form['password-repeat'] == '':
+            error = 'Password cannot be empty.'
+            return render_template("reg.html", title='Reg', error=error)
+        cx = sqlite3.connect(JDB_NAME)
+        cu = cx.cursor()
+        user_name = request.form['username']
+        password = request.form['password']
+        for t in [(user_name, password), ]:
+            sql_ins = "insert into sys_users(user_name,password) values (?,?)"
+            cu.execute(sql_ins, t)
+        cx.commit()
+        session['username'] = request.form['username']
+        #flash('Register Successfully.')
+        return redirect(url_for('index'))
     return render_template("reg.html", title='Reg')
 
 
@@ -48,5 +73,24 @@ def jManage(date=None):
         date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
     return render_template("jManage.html", title='jManage', datestr=date)
 
+
+def db_create():
+    cx = sqlite3.connect(JDB_NAME)
+    cu = cx.cursor()
+    sql_create = """create table if not exists sys_users (
+    user_id integer primary key,
+    user_group varchar(10),
+    user_name varchar(30),
+    password varchar(10),
+    email varchar(50),
+    telephone varchar(20),
+    status varchar(2) default '1',
+    create_date timestamp not null default current_timestamp
+    )"""
+    cu.execute(sql_create)
+    cx.commit()
+    cx.close()
+
 if __name__ == '__main__':
+    db_create()
     app.run(debug=True)
