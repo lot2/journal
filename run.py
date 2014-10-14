eae62ebd0c8668bf841a2298b872ea95852d09d1
lot.xu <754345848@qq.com>
2014-10-14 23:15:18 +0800

    20141014 add email,phone & add reg check(include exist user)

--- run.py
+++ run.py
@@ -51,21 +51,14 @@ app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 def reg():
     error = None
     if request.method == 'POST':
-        if request.form['password'] != request.form['password-repeat']:
-            error = 'Passwords do not match.'
-            return render_template("reg.html", title='Reg', error_reg=error)
-        if request.form['username'].strip() == '':
-            error = 'UserName cannot be empty.'
-            return render_template("reg.html", title='Reg', error_reg=error)
-        if request.form['password'] == '' or request.form['password-repeat'] == '':
-            error = 'Password cannot be empty.'
-            return render_template("reg.html", title='Reg', error_reg=error)
         cx = sqlite3.connect(JDB_NAME)
         cu = cx.cursor()
         user_name = request.form['username']
         password = request.form['password']
-        for t in [(user_name, password), ]:
-            sql_ins = "insert into sys_users(user_name,password) values (?,?)"
+        email = request.form['email']
+        telephone = request.form['telephone']
+        for t in [(user_name, password, email, telephone), ]:
+            sql_ins = "insert into sys_users(user_name,password,email,telephone) values (?,?,?,?)"
             cu.execute(sql_ins, t)
         cx.commit()
         session['username'] = request.form['username']
@@ -73,6 +66,23 @@ def reg():
         cx.close()
         return redirect(url_for('index'))
     return render_template("reg.html", title='Reg')
+
+
+@app.route('/existUser', methods=['GET', 'POST'])
+def existUser():
+    if request.method == "POST":
+        error = ""
+        cx = sqlite3.connect(JDB_NAME)
+        cu = cx.cursor()
+        user_name = request.form['username']
+        t = (user_name,)
+        sql = "select 1 from sys_users where user_name=?"
+        cu.execute(sql, t)
+        if cu.fetchone():
+            error = "UserName already exists."
+        cx.close()
+    return error
+
 
 
 @app.route('/new')
